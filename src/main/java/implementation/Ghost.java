862f4dfb39b4f555f2892d558b84a838cc9ab91a
chrisrauch193 <cr217@st-andrews.ac.uk>
2019-03-02 17:38:43 +0000

    Added simple ghost interection direction chagne ai

--- src/main/java/implementation/Ghost.java
+++ src/main/java/implementation/Ghost.java
@@ -12,6 +12,68 @@ public class Ghost extends Mover {
 
   @Override
   public void update(int[][] map) {
-    super.update(map);
+    convertPixelToMapPosition(DEFAULT_SCALE);
+    updatePixelPosition(map);
+  }
+
+  @Override
+  public void updatePixelPosition(int[][] map) {
+    switch(currentDirection) {
+      case NORTH:
+        northMovement(map);
+        break;
+      case EAST:
+        eastMovement(map);
+        break;
+      case SOUTH:
+        southMovement(map);
+        break;
+      case WEST:
+        westMovement(map);
+        break;
+      default:
+        System.out.println("I want to die :))");
+    }
+  }
+
+  @Override
+  public void northMovement(int[][] map) {
+    if (mapPosition.y > 0 && map[(int) mapPosition.x][(int) mapPosition.y] == 0) {
+      pixelPosition.y -= DEFAULT_SPEED;
+    } else {
+      makeGhostDirectionChoice();
+    }
+  }
+
+  @Override
+  public void eastMovement(int[][] map) {
+    if (mapPosition.x < map[0].length - 1 && map[(int) mapPosition.x][(int) mapPosition.y] == 0) {
+      pixelPosition.x += DEFAULT_SPEED;
+    } else {
+      makeGhostDirectionChoice();
+    }
+  }
+
+  @Override
+  public void southMovement(int[][] map) {
+    if (mapPosition.y < map.length - 1 && map[(int) mapPosition.x][(int) mapPosition.y] == 0) {
+      pixelPosition.y += DEFAULT_SPEED;
+    } else {
+      makeGhostDirectionChoice();
+    }
+  }
+
+  @Override
+  public void westMovement(int[][] map) {
+    if (mapPosition.x > 0 && map[(int) mapPosition.x][(int) mapPosition.y] == 0) {
+      pixelPosition.x -= DEFAULT_SPEED;
+    } else {
+      makeGhostDirectionChoice();
+    }
+  }
+
+  public void makeGhostDirectionChoice() {
+    // TODO: Make smarter. Add A* decision sometimes. Add choosing random not including current dirrection
+    currentDirection = Direction.randomDirection();
   }
 }
--- src/main/java/interfaces/Direction.java
+++ src/main/java/interfaces/Direction.java
@@ -1,5 +1,19 @@
 package interfaces;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum Direction {
-  NORTH, SOUTH, EAST, WEST
+  NORTH, SOUTH, EAST, WEST;
+
+  private static final List<Direction> DIRECTIONS =
+      Collections.unmodifiableList(Arrays.asList(values()));
+  private static final int SIZE = DIRECTIONS.size();
+  private static final Random RANDOM = new Random();
+
+  public static Direction randomDirection()  {
+    return DIRECTIONS.get(RANDOM.nextInt(SIZE));
+  }
 }
