d291491015a752c45771fdd75bb620a33a0458e7
chrisrauch193 <cr217@st-andrews.ac.uk>
2019-03-03 06:17:37 +0000

    Merge error fix

--- src/main/java/implementation/Player.java
+++ src/main/java/implementation/Player.java
@@ -2,6 +2,7 @@ package implementation;
 
 import static interfaces.Direction.*;
 
+import interfaces.Direction;
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
@@ -27,6 +28,7 @@ public class Player extends Mover {
     this.image = image;
     this.mapPosition = mapPosition;
     this.pixelPosition = new PVector();
+    this.lastDirection = SOUTH;
     this.currentDirection = SOUTH;
     this.convertMapToPixelPosition();
     this.imageMiddleN = imageMiddleN;
@@ -43,28 +45,36 @@ public class Player extends Mover {
 
   public void draw(PApplet app) {
     app.imageMode(PApplet.CORNER);
-    if(currentDirection == SOUTH) {
+    if (currentDirection == NONE) {
+      drawPacMan(app, lastDirection);
+    } else {
+      drawPacMan(app, currentDirection);
+      imageCounter++;
+    }
+    if(imageCounter > 30) imageCounter = 0;
+  }
+
+  public void drawPacMan(PApplet app, Direction currentMovingDirection) {
+    if(currentMovingDirection == SOUTH) {
       if(imageCounter / 10 == 1) app.image(this.image, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else if(imageCounter / 10 == 2) app.image(imageMiddleS, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else app.image(imageOpenS, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
     }
-    else if(currentDirection == NORTH) {
+    else if(currentMovingDirection == NORTH) {
       if(imageCounter / 10 == 1) app.image(this.image, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else if(imageCounter / 10 == 2) app.image(imageMiddleN, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else app.image(imageOpenN, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);;
     }
-    else if(currentDirection == WEST) {
+    else if(currentMovingDirection == WEST) {
       if(imageCounter / 10 == 1) app.image(this.image, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else if(imageCounter / 10 == 2) app.image(imageMiddleW, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else app.image(imageOpenW, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
     }
-    else if(currentDirection == EAST) {
+    else if(currentMovingDirection == EAST) {
       if(imageCounter / 10 == 1) app.image(this.image, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else if(imageCounter / 10 == 2) app.image(imageMiddleE, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
       else app.image(imageOpenE, pixelPosition.x, pixelPosition.y, Constants.SCALE, Constants.SCALE);
     }
-    imageCounter++;
-    if(imageCounter > 30) imageCounter = 0;
   }
 
   @Override
